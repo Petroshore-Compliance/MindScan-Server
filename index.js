d6db2db9da31523dea03d2e410fcc3587027927f
Andrei Faur <bogdan.andrei.faur@gmail.com>
2024-10-22 13:01:22 +0200

    Creación minima de funcionamiento

---
+++ index.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const { PORT } = process.env;
+const server = require('./src/app.js');
+const prisma = require('./src/db.js');
+
+server.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
+});
+
+process.on('SIGINT', async () => {
+  await prisma.$disconnect();
+  process.exit(0);
+});
