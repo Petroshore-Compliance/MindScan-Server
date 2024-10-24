ae0259d2612e288226b279340224cf92be42b620
Andrei Faur <bogdan.andrei.faur@gmail.com>
2024-10-24 10:18:12 +0200

    LoginUser y VerificationTokenUser
    Se ha creado la logica para el inicio de sesión del usuario que crea un token y luego se puede verificar con authMiddleware.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,21 @@
 const { Router } = require("express");
 const router = Router();
+
+const { registerUserMiddleware } = require("../middlewares/putUserMiddleware.js");
 const { registerUserHandler } = require("../handlers/registerUserHandler.js");
-const {
-  registerUserMiddleware,
-} = require("../middlewares/putUserMiddleware.js");
+const { loginUserHandler } = require("../handlers/loginUserHandler.js");
+const { authMiddleware } = require("../middlewares/authMiddleware.js");
 
 router.post("/register", registerUserMiddleware, registerUserHandler);
+router.post("/login", loginUserHandler);
+
+
+// Ruta para verificar si el token es válido (Solo para pruebas)
+router.post('/verify-user', authMiddleware, (req, res) => {
+    res.json({
+      message: 'Token válido',
+      user: req.user,
+    });
+  });
 
 module.exports = router;
