f630740d86d0f49f8a9855d20cd5d098ad2c9a45
Graxen <Romanraldhc1@gmail.com>
2024-10-31 09:43:06 +0100

    update profileUser
    Funcionalidad de modificar los parámetros de los usuarios

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
 const router = Router();
 
-const { authMiddleware } = require("../middlewares/authMiddleware.js");
 const { getProfileHandler } = require("../handlers/userHandlers/getProfileHandler.js");
+const { updateProfileHandler } = require("../handlers/userHandlers/updateProfileHandler.js");
 
-router.get("/me", authMiddleware, getProfileHandler);
+router.get("/me", getProfileHandler);
+router.patch("/update-profile", updateProfileHandler);
 
 module.exports = router;
