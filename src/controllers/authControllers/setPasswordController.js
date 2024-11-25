faf7f37101750193a962452441660f674deb9262
Graxen <Romanraldhc1@gmail.com>
2024-11-25 15:22:22 +0100

    set password middleware + code improvements
    Mejora de código de la función setPassword, aplicando corrección al middleware, controller y adaptando el test a los cambios, además de añadir comentarios para mayor claridad

--- src/controllers/authControllers/setPasswordController.js
+++ src/controllers/authControllers/setPasswordController.js
@@ -2,17 +2,11 @@ const bcrypt = require("bcrypt");
 
 const prisma = require("../../db.js");
 
-const regexPass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
-
-
+//cambiar la contraseña de un usuario que ha olvidado su contraseña
+//recibe el user_id y la nueva contraseña
+//y devuelve cambio de contraseña exitoso o fallido
 const setPasswordController = async (user_id, newPassword) => {
 
-  if(!user_id ){return {status: 400, message: "User ID is required."};}
-  if(!newPassword){return {status: 400, message: "New password is required."};}
-
-  if (!regexPass.test(newPassword)) {
-    return {status: 400, message: "Password must be at least 8 characters, include one uppercase letter, one lowercase letter, and one digit." };
-  }
 
 const user = await prisma.user.findUnique({
   where: { user_id: parseInt(user_id) },
