658c98e34f31a5ba032f4ebb895eb8f3e97c3240
Graxen <Romanraldhc1@gmail.com>
2024-12-23 11:56:27 +0100

    userTests fix
    actualización del los tests de userTests

--- src/__tests__/userTests/getProfileTest.js
+++ src/__tests__/userTests/getProfileTest.js
@@ -24,30 +24,19 @@ beforeAll(async () => {
     
 const userData = await prisma.user.findUnique({
   where: {       email: EMAIL_TESTER},
-  include: {
-    VerificationCodes: true
-  }
+  
 })
-
-    const verificationData = {
-      "user_id": userData.user_id,
-      "verificationCode": userData.VerificationCodes[0].code
-    }
-
-    const response2 = await request(app)
-      .get('/auth/verificate-user')
-      .send(verificationData);
 
       userId = userData.user_id;
 
-          const verificationData2 = {
+          const loginData = {
             "email": EMAIL_TESTER,
             "password": "secureHashedPassword123"
           }
       
           const response3 = await request(app)
             .post('/auth/login')
-            .send(verificationData2);
+            .send(loginData);
       
           if (response3.status !== 200) {
             console.log('Response body:', response3.body);
@@ -147,7 +136,6 @@ describe('Auth Endpoints', () => {
 
 // borrado de lo creado
 afterAll(async () => {
-  await prisma.verificationCode.deleteMany();
   await prisma.user.deleteMany();
   await prisma.company.deleteMany();
       
