ee6d1328646e827f54723f0ade5ffff1571a3f79
Graxen <Romanraldhc1@gmail.com>
2025-01-23 09:21:51 +0100

    GetContactForm
    lógica de getcontactform y solución de error tipográfico

--- src/middlewares/contactFormMiddlewares/getContactFormMiddleware.js
+++ src/middlewares/contactFormMiddlewares/getContactFormMiddleware.js
@@ -1,7 +1,22 @@
+const { validateNumber } = require("../../tools/validations.js");
+
 const getContactFormMiddleware = async (req, res, next) => {
 
-  next();
-  
+  let errors = [];
+  let result;
+  let { form_id } = req.body;
+
+  if (form_id) {
+    result = validateNumber(form_id, "form_id");
+    if (result.error) errors.push(result.error)
   }
-  
-  module.exports = { getContactFormMiddleware };
+  if (errors.length === 0) {
+    next();
+  }
+  else {
+    res.status(400).json({ errors });
+  }
+
+}
+
+module.exports = { getContactFormMiddleware };
