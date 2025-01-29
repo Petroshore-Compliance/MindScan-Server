595b1b2ca577c0ef5910e998e9706fae3e783840
Graxen <Romanraldhc1@gmail.com>
2025-01-29 11:36:09 +0100

    email sent when created contact form
    Modificación del código para que se envié un email a los petroadmin que tienen "notificaciones" en true

--- src/controllers/contactFormControllers/createContactFormController.js
+++ src/controllers/contactFormControllers/createContactFormController.js
@@ -3,6 +3,8 @@ const prisma = require("../../db.js");
 //comprobación de si hace falta la creación del contactform
 const { createVerificationScript } = require("../../tools/createVerificationScript.js");
 
+const { emailContactFormCreatedScript } = require("../../tools/emailContactFormCreatedScript.js");
+
 
 
 const createContactFormController = async (data) => {
@@ -15,12 +17,12 @@ const createContactFormController = async (data) => {
       created_at: "desc",
     },
   });
-if (alreadyExist) {
-  const noSeDebeEnviar = await createVerificationScript(alreadyExist.state);
-  if(noSeDebeEnviar.reason){
-    return { status: noSeDebeEnviar.status, message: noSeDebeEnviar.reason };
+  if (alreadyExist) {
+    const noSeDebeEnviar = await createVerificationScript(alreadyExist.state);
+    if (noSeDebeEnviar.reason) {
+      return { status: noSeDebeEnviar.status, message: noSeDebeEnviar.reason };
+    }
   }
-}
 
   const form = await prisma.contactForm.create({
     data: {
@@ -31,6 +33,9 @@ if (alreadyExist) {
       message: data.message,
     },
   });
+
+
+  await emailContactFormCreatedScript(form.name, form.email, form.phone, form.language, form.message, "../templates/ContactFormCreatedEmail.html");
 
   return { status: 201, message: "Form created successfully", form: form };
 };
