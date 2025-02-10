72e71b242f842b99734dcc4388cb996dea379782
Youssef <220238@fppiramide.com>
2025-02-10 16:43:02 +0100

    User Created Template
    Plantilla de usuario registrado

--- src/tools/emailCreatedUserScript.js
+++ src/tools/emailCreatedUserScript.js
@@ -2,6 +2,8 @@ const prisma = require("../db.js");
 const sendEmail = require("./nodemailer.js");
 const fs = require("fs");
 const path = require("path");
+const juice = require("juice");
+
 
 const emailCreatedUserScript = async (
   name,
@@ -16,10 +18,17 @@ const emailCreatedUserScript = async (
 
   htmlTemplate = fs.readFileSync(htmlTemplatePath, "utf8");
 
-  const htmlContent = htmlTemplate.replace(/{{name}}/g, name);
+  const htmlContent = htmlTemplate
+  .replace(/{{name}}/g, name)
+  .replace(/{{HOMEPAGEURL}}/g, process.env.HOMEPAGEURL);
 
+  //Obtener css
+  const globalCSS = fs.readFileSync(path.join(__dirname, "../styles/global.css"), "utf-8");
+  
+  // Aplicar Juice para estilos inline (SIN modificar la plantilla original)
+  const inlinedHtml = juice.inlineContent(htmlContent, globalCSS);
 
-  await sendEmail(email, subject, htmlContent);
+  await sendEmail(email, subject, inlinedHtml);
 
   return `The User with id: "${name}" has been invited to the company with id: "${companyName}"`;
 };
