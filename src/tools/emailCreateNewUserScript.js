3c28d3cadb7f04402458a565e4473a9139220f1b
Youssef <220238@fppiramide.com>
2025-02-10 10:42:29 +0100

    InvitatioNewAccountHTML
    Plantilla con css añadido a través de Juice

--- src/tools/emailCreateNewUserScript.js
+++ src/tools/emailCreateNewUserScript.js
@@ -2,31 +2,32 @@ const prisma = require("../db.js");
 const sendEmail = require("./nodemailer.js");
 const fs = require("fs");
 const path = require("path");
+const juice = require("juice");
 
 const emailCreateNewUserScript = async (
   emailToVerify,
   htmlPath,
-  companyName,
+  name,
   verificationToken
 ) => {
-  const subject = "Invited to another company";
+  const subject = "You have been invited to join a company";
 
   const htmlTemplatePath = path.join(__dirname, htmlPath);
-  let htmlTemplate;
+  
+  let htmlTemplate = fs.readFileSync(htmlTemplatePath, "utf8");
 
-  htmlTemplate = fs.readFileSync(htmlTemplatePath, "utf8");
+  const htmlContent = htmlTemplate
+  .replace(/{{joinCompanyURL}}/g, "localhost:400/pestañaDonderegistrarse")
+  .replace(/{{newAccountToken}}/g, verificationToken)
+  .replace(/{{company}}/g, name ? name : "Petroshore Compliance");
 
-  const htmlContent3 = htmlTemplate.replace(
-    /{{joinCompanyURL}}/g,
-    "localhost:400/pestañaDonderegistrarse"
-  );
-  const htmlContent2 = htmlContent3.replace(/{{newAccountToken}}/g, verificationToken);
-  const htmlContent = htmlContent2.replace(
-    /{{company}}/g,
-    companyName ? companyName : "Petroshore Compliance"
-  );
+  //Obtener css
+  const globalCSS = fs.readFileSync(path.join(__dirname, "../styles/global.css"), "utf-8");
+  
+  // Aplicar Juice para estilos inline (SIN modificar la plantilla original)
+  const inlinedHtml = juice.inlineContent(htmlContent, globalCSS);
 
-  sendEmail(emailToVerify, subject, htmlContent);
+  await sendEmail(emailToVerify, subject, inlinedHtml);
 
   return `User invited succesfully`;
 };
